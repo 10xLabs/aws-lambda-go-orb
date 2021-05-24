8e222a184296a6f9e4e1ab8132cd00c7bebf2e1b
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-05-24 16:07:45 -0500

    Initial orb version

---
+++ src/scripts/check_coverage.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install -y bc
+CURRENT_COVERAGE=$(go tool cover -func cover.out | grep total | awk '{print substr($3, 1, length($3)-1)}')
+exit $(echo "$CURRENT_COVERAGE < $MINIMUM_COVERAGE" | bc)
---
+++ src/scripts/export_coverage.sh
@@ -0,0 +1,2 @@
+mkdir -p $ARTIFACTS_FOLDER
+go tool cover -html=cover.out -o $ARTIFACTS_FOLDER/coverage.html
