587df71c9397d84ba9deb526cfbce256f35f36cc
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-06-03 21:25:55 -0500

    update build

--- src/scripts/build.sh
+++ src/scripts/build.sh
@@ -1,3 +1,1 @@
-echo "$INPUT_FILE"
-echo "$OUTPUT_FILE"
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build "$INPUT_FILE" -v -ldflags="-w -s" -o "$OUTPUT_FILE"
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -v -ldflags="-w -s" -o "$OUTPUT_FILE" "$INPUT_FILE"
