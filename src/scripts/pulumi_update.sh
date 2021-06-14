14d4f0c137ce171ee6376b70d790a66f2a9890ad
blablo <eduardo@nexbus.com.mx>
2021-06-14 15:29:53 -0400

    Add pulumi init

--- src/scripts/pulumi_update.sh
+++ src/scripts/pulumi_update.sh
@@ -1,1 +1,7 @@
-pulumi update --stack "$STACK_NAME" --cwd pulumi --yes
+if pulumi stack ls --cwd pulumi | grep -q "$STACK_NAME"; then
+echo "exists"
+else
+echo "create"
+pulumi stack init "$STACK_NAME" --secrets-provider="awskms://alias/staging-PulumiSecrets?region=us-east-1" --cwd pulumi
+fi
+pulumi update --stack "$STACK_NAME" --secrets-provider="awskms://alias/staging-PulumiSecrets?region=us-east-1" --cwd pulumi --yes
