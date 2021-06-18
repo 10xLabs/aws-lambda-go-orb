f7351461419d6f4e44201bd6bf77a146b2ff418b
blablo <eduardo@nexbus.com.mx>
2021-06-18 13:21:42 -0400

    [semver:patch] remove secret provider

--- src/scripts/pulumi_update.sh
+++ src/scripts/pulumi_update.sh
@@ -2,6 +2,6 @@ if pulumi stack ls --cwd pulumi | grep -q ^"$STACK_NAME"; then
 echo "exists"
 else
 echo "create"
-pulumi stack init "$STACK_NAME" --secrets-provider="awskms://alias/staging-PulumiSecrets?region=us-east-1" --cwd pulumi
+pulumi stack init "$STACK_NAME" --cwd pulumi
 fi
-pulumi update --stack "$STACK_NAME" --secrets-provider="awskms://alias/staging-PulumiSecrets?region=us-east-1" --cwd pulumi --yes
+pulumi update --stack "$STACK_NAME" --cwd pulumi --yes
