a3aa3e53232e37414d1ac30f18521fa5d4a513c3
jbpineiroc <jbpineiroc@gmail.com>
2021-06-20 16:02:37 -0500

    [semver:minor] Update Pulumi directory 3.2.0 (#8)
    * Add build job params (#7)
    
    * change default pulumi directory to deploy
    
    Co-authored-by: Yaniel Velázquez <yaniel.alfred@gmail.com>

--- src/scripts/pulumi_update.sh
+++ src/scripts/pulumi_update.sh
@@ -1,7 +1,7 @@
-if pulumi stack ls --cwd pulumi | grep -q ^"$STACK_NAME"; then
+if pulumi stack ls --cwd deploy | grep -q ^"$STACK_NAME"; then
 echo "exists"
 else
 echo "create"
-pulumi stack init "$STACK_NAME" --cwd pulumi
+pulumi stack init "$STACK_NAME" --cwd deploy
 fi
-pulumi update --stack "$STACK_NAME" --cwd pulumi --yes
+pulumi update --stack "$STACK_NAME" --cwd deploy --yes
