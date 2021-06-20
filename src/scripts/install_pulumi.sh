f5d5eef73b50edd70fdca144729ecf99d2747ff1
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-06-20 15:42:46 -0500

    change default pulumi directory to deploy

--- src/scripts/install_pulumi.sh
+++ src/scripts/install_pulumi.sh
@@ -9,7 +9,7 @@ echo "export PATH=${HOME}/.pulumi/bin:$PATH" >> "$BASH_ENV"
 # shellcheck source=/dev/null
 source "$BASH_ENV"
 
-cd pulumi || exit
+cd deploy || exit
 echo  "//npm.pkg.github.com/:_authToken=$NPM_GITHUB_TOKEN" > ./.npmrc
 echo  "registry=$NPM_GITHUB_REGISTRY" >> ./.npmrc
 npm install
