ba8a4beb94c46f3e95ffaa3da52563aea2dc2330
blablo <eduardo@nexbus.com.mx>
2021-06-30 09:37:13 -0400

    print coverages

--- src/scripts/check_coverage.sh
+++ src/scripts/check_coverage.sh
@@ -2,7 +2,7 @@ sudo apt-get update
 sudo apt-get install -y bc
 CURRENT_COVERAGE=$(GOFLAGS='-mod=vendor' go tool cover -func cover.out | grep total | awk '{print substr($3, 1, length($3)-1)}')
 echo "current '$CURRENT_COVERAGE' minimum '$MINIMUM_COVERAGE'"
-if [ $CURRENT_COVERAGE == "100.0" ]; then
+if [ "$CURRENT_COVERAGE" == "100.0" ]; then
   exit 0
 fi
 exit "$(echo "$CURRENT_COVERAGE < $MINIMUM_COVERAGE" | bc)"
