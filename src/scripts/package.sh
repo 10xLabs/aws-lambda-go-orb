40dcbd75beed96af506fd94fdbdaab54e9193b26
Yaniel Velázquez <yaniel.alfred@gmail.com>
2021-06-30 10:29:48 -0500

    [semver:minor] feat(package): improve script (#12)

--- src/scripts/package.sh
+++ src/scripts/package.sh
@@ -1,1 +1,5 @@
-zip "$OUTPUT_FILE" "$INPUT_FILE"
+# shellcheck disable=SC2153
+input_parentdir="$(dirname "$INPUT_FILE")"
+input_file="$(basename "$INPUT_FILE")"
+output_realpath="$(realpath "$OUTPUT_FILE")"
+cd "$input_parentdir" && zip "$output_realpath" "$input_file"
