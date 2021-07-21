6565d62a2097b5d9e97e59eb613e8c65ef65c98b
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-07-21 02:20:58 -0500

    chore: add logs

--- src/scripts/tag_jira_issues.sh
+++ src/scripts/tag_jira_issues.sh
@@ -1,8 +1,12 @@
 COMMIT_BODY=$(git log -1 origin/master --pretty=format:%b)
+echo "$COMMIT_BODY"
+echo "@@"
 # shellcheck disable=SC2206
 IFS=$'\n' COMMIT_MESSAGES=($COMMIT_BODY)
 for COMMIT_MESSAGE in "${COMMIT_MESSAGES[@]}"
 do
+    echo "$COMMIT_MESSAGE"
+    echo "@@"
     if [[ "$COMMIT_MESSAGE" =~ \[([A-Z]{2,3}-[0-9]+)\] ]]; then
         ISSUE_ID="${BASH_REMATCH[1]}"
         curl --request PUT \
