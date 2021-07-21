8a9d6ebd9e9c798502e5a653e012ac1f8a0b597f
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-07-21 01:30:57 -0500

    feat: add tags jira issues

---
+++ src/scripts/tag_jira_issues.sh
@@ -0,0 +1,26 @@
+COMMIT_BODY=$(git log -1 origin/master --pretty=format:%b)
+# shellcheck disable=SC2206
+COMMIT_MESSAGES=(${COMMIT_BODY//\n/ })
+for COMMIT_MESSAGE in "${COMMIT_MESSAGES[@]}"
+do
+    if [[ "$COMMIT_MESSAGE" =~ \[([A-Z]{2,3}-[0-9]+)\] ]]; then
+        ISSUE_ID="${BASH_REMATCH[1]}"
+        curl --request PUT \
+          --url "https://$JIRA_DOMAIN.atlassian.net/rest/api/3/issue/$ISSUE_ID" \
+          --user "$JIRA_USER:$JIRA_TOKEN" \
+          --header "Accept: application/json" \
+          --header "Content-Type: application/json" \
+          --data "{
+                    \"update\": {
+                      \"labels\": [
+                        {
+                          \"add\": \"$RELEASE_TAG\"
+                        },
+                        {
+                          \"add\": \"$CIRCLE_PROJECT_REPONAME\"
+                        }
+                      ]
+                    }
+                  }"
+    fi
+done
