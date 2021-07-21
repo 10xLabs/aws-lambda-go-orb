ae5b2153a0d19eb786c33f2335a791281f2de352
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-07-21 01:04:41 -0500

    fix: update quotes mark

--- src/scripts/tag_jira_issue.sh
+++ src/scripts/tag_jira_issue.sh
@@ -2,19 +2,19 @@ ISSUE_ID="NA-2175"
 RELEASE_TAG="v1.0.0"
 
 curl --request PUT \
-  --url 'https://$JIRA_DOMAIN.atlassian.net/rest/api/3/issue/$ISSUE_ID' \
-  --user '$JIRA_USER:$JIRA_TOKEN' \
-  --header 'Accept: application/json' \
-  --header 'Content-Type: application/json' \
-  --data '{
-            "update": {
-              "labels": [
+  --url "https://$JIRA_DOMAIN.atlassian.net/rest/api/3/issue/$ISSUE_ID" \
+  --user "$JIRA_USER:$JIRA_TOKEN" \
+  --header "Accept: application/json" \
+  --header "Content-Type: application/json" \
+  --data "{
+            \"update\": {
+              \"labels\": [
                 {
-                  "add": "$RELEASE_TAG"
+                  \"add\": \"$RELEASE_TAG\"
                 },
                 {
-                  "add": "$CIRCLE_PROJECT_REPONAME"
+                  \"add\": \"$CIRCLE_PROJECT_REPONAME\"
                 }
               ]
-            },
-          }'
+            }
+          }"
