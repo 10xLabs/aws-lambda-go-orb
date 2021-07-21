ded5cd50952dba93f16fdd06025d177023ef5388
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-07-21 02:32:39 -0500

    chore: remove logs

--- src/scripts/tag_jira_issues.sh
+++ src/scripts/tag_jira_issues.sh
@@ -1,15 +1,8 @@
 COMMIT_BODY=$(git log -1 origin/master --pretty=format:%b)
-echo "$COMMIT_BODY"
-echo "@@"
 # shellcheck disable=SC2206
 IFS=$'\n' COMMIT_MESSAGES=($COMMIT_BODY)
 for COMMIT_MESSAGE in "${COMMIT_MESSAGES[@]}"
 do
-    echo "$COMMIT_MESSAGE"
-    echo "@@"
-    COMMIT_MESSAGE="$COMMIT_MESSAGE [NA-2272]"
-    echo "$COMMIT_MESSAGE"
-    echo "@@"
     if [[ "$COMMIT_MESSAGE" =~ \[([A-Z]{2,3}-[0-9]+)\] ]]; then
         ISSUE_ID="${BASH_REMATCH[1]}"
         curl --request PUT \
