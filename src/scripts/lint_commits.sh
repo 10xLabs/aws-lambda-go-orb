27d80a1d08f134869b9cea3a33827d987c2a6cca
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-07-26 12:27:46 -0500

    chore: update pipeline

--- src/scripts/lint_commits.sh
+++ src/scripts/lint_commits.sh
@@ -1,4 +1,6 @@
 COMMITS=$(git log "origin/$CIRCLE_BRANCH..origin/develop" --pretty=format:%s)
+echo "$COMMITS"
+echo "@@"
 # shellcheck disable=SC2206
 COMMIT_MESSAGES=(${COMMITS//\n/ })
 for COMMIT_MESSAGE in "${COMMIT_MESSAGES[@]}"
