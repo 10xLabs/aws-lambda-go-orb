9455235793c34329483c714d009e4808dabdc9fe
blablo <eduardo@nexbus.com.mx>
2021-09-01 15:26:30 -0400

    [semver:patch] fix: init stack if doesn't exists

--- src/scripts/pulumi_update.sh
+++ src/scripts/pulumi_update.sh
@@ -2,6 +2,6 @@ if pulumi stack ls --cwd "$WORKING_DIRECTORY" | grep -q ^"$STACK_NAME"; then
 echo "stack exists"
 else
 echo "init stack"
-pulumi stack init "$STACK_NAME" --cwd "$WORKING_DIRECTORY" --yes
+pulumi stack init "$STACK_NAME" --cwd "$WORKING_DIRECTORY"
 fi
 pulumi update --stack "$STACK_NAME" --cwd "$WORKING_DIRECTORY" --yes
