3aad1e9e10ddb862b5ef9eeef2e1851cd6b6d598
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-10-22 14:17:58 -0500

    test: ci

--- src/scripts/commit_documentation.sh
+++ src/scripts/commit_documentation.sh
@@ -4,7 +4,8 @@ git config --global user.email "circleci@nexbus.com"
 git clone "https://$GITHUB_USER:$GITHUB_PAT@github.com/10xLabs/nexdocs.git"
 
 cd nexdocs
-git checkout "$CIRCLE_BRANCH"
+# git checkout "$CIRCLE_BRANCH"
+git checkout develop
 
 FILE_NAME=$(echo "$MODULE_NAME" | tr '[:upper:]' '[:lower:]')
 mv "$DOCUMENTATION_FILE" "./aggregates/$AGGREGATE_NAME/$FILE_NAME.md"
