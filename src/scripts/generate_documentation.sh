175e46cb975a26f4ea69aab9febcfb4aedeb2d2b
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-10-22 14:16:42 -0500

    test: ci

--- src/scripts/generate_documentation.sh
+++ src/scripts/generate_documentation.sh
@@ -1,8 +1,8 @@
 export GOPRIVATE="github.com/10xLabs"
 go install github.com/10xLabs/gomarkdoc/cmd/gomarkdoc@latest
 
-mkdir docs
+mkdir -p /tmp/docs
 FILE_NAME=$(echo "$MODULE_NAME" | tr '[:upper:]' '[:lower:]')
-gomarkdoc "./$MODULE_PATH" -t func= > "docs/$FILE_NAME.md"
+gomarkdoc "./$MODULE_PATH" -t func= > "/tmp/docs/$FILE_NAME.md"
 
-echo "export DOCUMENTATION_FILE=docs/$FILE_NAME.md" >> "$BASH_ENV"
+echo "export DOCUMENTATION_FILE=/tmp/docs/$FILE_NAME.md" >> "$BASH_ENV"
--- src/scripts/install_aws_cli.sh
+++
@@ -1,4 +0,0 @@
-curl -sSL "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip -q -o awscliv2.zip
-sudo ./aws/install
-rm awscliv2.zip
