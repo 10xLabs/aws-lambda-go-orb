68ff26ab909307cd065233cc6fff8210a2df1abb
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-10-22 13:17:09 -0500

    test: ci

--- src/scripts/generate_documentation.sh
+++ src/scripts/generate_documentation.sh
@@ -1,6 +1,11 @@
 export GOPRIVATE="github.com/10xLabs"
 go install github.com/10xLabs/gomarkdoc/cmd/gomarkdoc@latest
 go env
-ls -al "$GOROOT/bin"
-ls -al "$GOPATH/bin"
-gomarkdoc
+# ls -al "$GOROOT/bin"
+# ls -al "$GOPATH/bin"
+# gomarkdoc
+
+FILE_NAME=$(echo "$MODULE_NAME" | tr '[:upper:]' '[:lower:]')
+gomarkdoc "$MODULE_PATH" -t func= > "docs/$FILE_NAME.md"
+
+ls -al docs
