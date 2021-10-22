5d72c8831f21a4bd15e40e058f29da799d9bffaa
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2021-10-22 14:26:38 -0500

    test: ci

--- src/scripts/commit_documentation.sh
+++ src/scripts/commit_documentation.sh
@@ -1,9 +1,9 @@
-cd /tmp
+cd /tmp || exit
 git config --global user.name "Circle CI"
 git config --global user.email "circleci@nexbus.com"
 git clone "https://$GITHUB_USER:$GITHUB_PAT@github.com/10xLabs/nexdocs.git"
 
-cd nexdocs
+cd nexdocs || exit
 # git checkout "$CIRCLE_BRANCH"
 git checkout develop
 
