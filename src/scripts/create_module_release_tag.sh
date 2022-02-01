dfbd4b386ba1303ac0a0754dd342ea47fbf80e18
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2022-02-01 15:50:24 -0600

    chore: test ci

--- src/scripts/create_module_release_tag.sh
+++ src/scripts/create_module_release_tag.sh
@@ -1,4 +1,10 @@
 # shellcheck disable=SC2148
+# shellcheck disable=SC2046
+if [ $(git tag -l "$RELEASE_TAG") ]; then
+    circleci-agent step halt
+    exit 0
+fi
+
 git config --global user.name "Circle CI"
 git config --global user.email "circleci@nexbus.com.mx"
 git remote set-url origin "https://$GITHUB_USER:$GITHUB_PAT@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git"
