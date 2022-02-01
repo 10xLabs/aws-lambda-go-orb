c34bee14afe1e694ec39af660fa4dac2f4e1f56d
Blablo <eduardo@nexbus.com.mx>
2022-02-01 10:32:51 -0500

    fix: update src/scripts/create_module_release_tag.sh

--- src/scripts/create_module_release_tag.sh
+++ src/scripts/create_module_release_tag.sh
@@ -1,6 +1,6 @@
 # shellcheck disable=SC2148
 git config --global user.name "Circle CI"
-git config --global user.email "circleci@nexbus.com"
+git config --global user.email "circleci@nexbus.com.mx"
 git remote set-url origin "https://$GITHUB_USER:$GITHUB_PAT@github.com/$CIRCLE_PROJECT_USERNAME/$CIRCLE_PROJECT_REPONAME.git"
 
 git tag -a "$RELEASE_TAG" -m "release $RELEASE_TAG"
