5b33b7bfb12b5d926ca825b22033168466359405
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2022-03-16 19:05:03 -0600

    test: ci/cd

--- src/scripts/calculate_module_release_version.sh
+++ src/scripts/calculate_module_release_version.sh
@@ -18,6 +18,7 @@ tags+=("$ZERO_VERSION")
 for tag in "${tags[@]}"
 do
     echo "tag: $tag"
+    echo "pre_version: $pre_version"
     if [[ $tag =~ ^$MODULE_NAME/v[0-9]+.[0-9]+.[0-9]+$ ]]; then
         version=${tag#"$MODULE_NAME/v"}
         # shellcheck disable=SC2206
