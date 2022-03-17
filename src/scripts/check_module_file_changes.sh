b2fe7a299df44714f2331d7757a05d81d6a5c6b4
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2022-03-16 18:27:45 -0600

    test: ci/cd

--- src/scripts/check_module_file_changes.sh
+++ src/scripts/check_module_file_changes.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2148
 FILES=$(git diff --name-only HEAD~1..HEAD "$MODULE_PATH")
 
-# if [ -z "$FILES" ]; then
-#     circleci-agent step halt
-#     exit 0
-# fi
+if [ -z "$FILES" ]; then
+    circleci-agent step halt
+    exit 0
+fi
