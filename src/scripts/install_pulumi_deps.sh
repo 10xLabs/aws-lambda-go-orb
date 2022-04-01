5a8829c2cdab264c17547f1ce17221d0e06d9427
blablo <blablo@gmail.com>
2022-04-01 16:00:50 -0400

    chore: test

--- src/scripts/install_pulumi_deps.sh
+++ src/scripts/install_pulumi_deps.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 cd "$WORKING_DIRECTORY" || exit
+
 echo "registry=https://registry.npmjs.org/" >> ./.npmrc
 echo  "@10xLabs:registry=$NPM_GITHUB_REGISTRY" >> ./.npmrc
 echo  "//npm.pkg.github.com/:_authToken=$GITHUB_PAT" > ./.npmrc
 
+npm install
