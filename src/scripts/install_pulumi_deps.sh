d2ef72dc8312735d4ebff4812c85509720905bd7
jbpineiroc <jbpineiroc@gmail.com>
2022-05-19 11:26:19 -0500

    [semver:minor] feat: add yarn support for Pulumi

--- src/scripts/install_pulumi_deps.sh
+++ src/scripts/install_pulumi_deps.sh
@@ -5,4 +5,8 @@ echo  "//npm.pkg.github.com/:_authToken=$GITHUB_PAT" > ./.npmrc
 echo  "@10xLabs:registry=$NPM_GITHUB_REGISTRY" >> ./.npmrc
 echo "registry=https://registry.npmjs.org" >> ./.npmrc
 
-npm install
+if test -f "yarn.lock"; then
+    yarn install
+else
+    npm install
+fi
