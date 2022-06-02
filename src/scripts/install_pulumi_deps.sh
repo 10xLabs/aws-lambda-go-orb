d436652fc5a4f1796f911600328a0fd0175f58c9
jbpineiroc <jbpineiroc@gmail.com>
2022-06-02 11:34:06 -0500

    [semver:patch] fix: yarn install from private repository

--- src/scripts/install_pulumi_deps.sh
+++ src/scripts/install_pulumi_deps.sh
@@ -2,11 +2,15 @@
 cd "$WORKING_DIRECTORY" || exit
 
 echo  "//npm.pkg.github.com/:_authToken=$GITHUB_PAT" > ./.npmrc
-echo  "@10xLabs:registry=$NPM_GITHUB_REGISTRY" >> ./.npmrc
-echo "registry=https://registry.npmjs.org" >> ./.npmrc
 
 if test -f "yarn.lock"; then
+    echo  '"@10xLabs:registry" "https://npm.pkg.github.com"' > ./.yarnrc
+    echo 'registry "https://registry.npmjs.org"' >> ./.yarnrc
+    
     yarn install
 else
+    echo  "@10xLabs:registry=$NPM_GITHUB_REGISTRY" >> ./.npmrc
+    echo "registry=https://registry.npmjs.org" >> ./.npmrc
+    
     npm install
 fi
