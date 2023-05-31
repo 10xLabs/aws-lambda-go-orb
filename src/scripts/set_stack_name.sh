e8182c9d56ca7ba9b4664de3f1271bd894274dd9
Yaniel Velázquez <yaniel.alfred@gmail.com>
2023-05-30 18:26:43 -0600

    release v3.18.0

--- src/scripts/set_stack_name.sh
+++ src/scripts/set_stack_name.sh
@@ -1,2 +1,6 @@
 # shellcheck disable=SC2148
-echo "export STACK_NAME=$CIRCLE_PROJECT_REPONAME.$ENVIRONMENT" >> "$BASH_ENV"
+if [ -n "$STACK_NAME" ]; then
+    echo "export STACK_NAME=$STACK_NAME" >> "$BASH_ENV"
+else
+    echo "export STACK_NAME=$CIRCLE_PROJECT_REPONAME.$ENVIRONMENT" >> "$BASH_ENV"
+fi
