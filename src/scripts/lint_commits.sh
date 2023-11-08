f03c679e20fcc59b99717e997295eff0a725990f
jbpineiroc <jbpineiroc@gmail.com>
2023-11-08 10:28:01 -0600

    release v3.20.4

--- src/scripts/lint_commits.sh
+++ src/scripts/lint_commits.sh
@@ -2,8 +2,10 @@
 COMMITS=$(git log "origin/develop..origin/$CIRCLE_BRANCH" --pretty=format:%s)
 # shellcheck disable=SC2206
 IFS=$'\n' COMMIT_MESSAGES=($COMMITS)
-for COMMIT_MESSAGE in "${COMMIT_MESSAGES[@]}"
-do
-    echo "$COMMIT_MESSAGE"
+for COMMIT_MESSAGE in "${COMMIT_MESSAGES[@]}"; do
+    if [[ $COMMIT_MESSAGE == release* ]]; then
+        continue
+    fi
+
     echo "$COMMIT_MESSAGE" | npx commitlint
 done
