2a2a733818449f35bd450c7b029f98b720dd5c1d
Jorge Bárbaro Piñeiro Cruz <jbpineiroc@gmail.com>
2023-11-16 15:23:19 -0600

    chore: skip lint from master

--- src/scripts/lint_commits.sh
+++ src/scripts/lint_commits.sh
@@ -1,11 +1,9 @@
-# shellcheck disable=SC2148
-COMMITS=$(git log "origin/develop..origin/$CIRCLE_BRANCH" --pretty=format:%s)
-# shellcheck disable=SC2206
-IFS=$'\n' COMMIT_MESSAGES=($COMMITS)
-for COMMIT_MESSAGE in "${COMMIT_MESSAGES[@]}"; do
-    if [[ $COMMIT_MESSAGE == release* ]]; then
-        continue
-    fi
-
-    echo "$COMMIT_MESSAGE" | npx commitlint
-done
+if [[ "$CIRCLE_BRANCH" != "master" ]]; then
+    # shellcheck disable=SC2148
+    COMMITS=$(git log "origin/develop..origin/$CIRCLE_BRANCH" --pretty=format:%s)
+    # shellcheck disable=SC2206
+    IFS=$'\n' COMMIT_MESSAGES=($COMMITS)
+    for COMMIT_MESSAGE in "${COMMIT_MESSAGES[@]}"; do
+        echo "$COMMIT_MESSAGE" | npx commitlint
+    done
+fi
