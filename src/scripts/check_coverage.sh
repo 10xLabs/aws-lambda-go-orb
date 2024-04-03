216160df97492a9543b976d3f76e1baf8e78c9f9
Arlen Toledano <a.toledano@kolors.com.mx>
2024-04-03 08:47:59 -0600

    fix: rm comment

--- src/scripts/check_coverage.sh
+++ src/scripts/check_coverage.sh
@@ -1,8 +1,5 @@
 # shellcheck disable=SC2148
 
-# we may use this again when go version 1.22 toolchain gets fixed
-#CURRENT_COVERAGE=$(GOEXPERIMENT='nocoverageredesign' GOFLAGS='-mod=vendor' go tool cover -func cover.out | grep total | tail -n 1 | awk '{print substr($3, 1, length($3)-1)}')
-
 CURRENT_COVERAGE=$(GOEXPERIMENT='nocoverageredesign' GOFLAGS='-mod=vendor' go test --cover ./... | grep -Eo '[0-9]+\.[0-9]+% of statements' | awk -F'%' '{print $1}' | awk '{ sum += $1 } END { print sum/NR }')
 CURRENT_COVERAGE=${CURRENT_COVERAGE%.*}
 MINIMUM_COVERAGE=${MINIMUM_COVERAGE%.*}
