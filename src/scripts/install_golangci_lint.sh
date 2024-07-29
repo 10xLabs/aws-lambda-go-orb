2122690ec55c340cdc52bf10c7d9508ebf7931b3
blablo <blablo@gmail.com>
2024-07-29 12:14:18 -0400

    fix: customer site changed

--- src/scripts/install_golangci_lint.sh
+++ src/scripts/install_golangci_lint.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 cd "$WORKING_DIRECTORY" || exit
-
+sudo npm install -g @commitlint/cli @commitlint/config-conventional
 curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.58.0
