2c115b2aab1e151f9a615ca738612e34915bd8f9
blablo <blablo@gmail.com>
2024-07-30 16:18:05 -0400

    fix: customer site changed

--- src/scripts/check_coverage.sh
+++ src/scripts/check_coverage.sh
@@ -1,5 +1,5 @@
-# shellcheck disable=SC2148
-CURRENT_COVERAGE=$(GOEXPERIMENT='nocoverageredesign' GOFLAGS='-mod=vendor' go test --cover ./... | grep -Eo '[0-9]+\.[0-9]+% of statements' | awk -F'%' '{print $1}' | awk '{ sum += $1 } END { print sum/NR }')
+#!/bin/bash
+CURRENT_COVERAGE=$(GOEXPERIMENT='nocoverageredesign' GOFLAGS='-mod=vendor' go test -coverprofile=coverage.out -parallel $(nproc) ./... | awk '{ if ($1 ~ /coverage:/) { sum += $2; count++ }} END { print sum/count }')
 CURRENT_COVERAGE=${CURRENT_COVERAGE%.*}
 MINIMUM_COVERAGE=${MINIMUM_COVERAGE%.*}
 
